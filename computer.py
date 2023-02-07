695d498ac395d1c7669d7d06adbe30b58e981cdc
Sophia Manodori <smanodori@smith.edu>
2023-02-07 14:35:50 -0500

    added memory

--- computer.py
+++ computer.py
@@ -1,26 +1,28 @@
 class Computer:
 
     # Attributes
-    id: int = 0
+    id: str = ""
     year: int = 0
     opsystem: str = ""
     value: float = 0.0
+    memory: int = 0
     
     # Constructor
-    def __init__(self, name: str, age: int, system:str, starting_price: float):
+    def __init__(self, name: str, age: int, system:str, starting_price: float, memory: int):
          # You'll remove this when you fill out your constructor
         self.id = name
         self.year = age
         self.opsystem = system
         self.value=starting_price
+        self.memory = memory
     # Methods
     #prints details
     def printDetails(self):
         print("Name:", self.id)
         print("OS:", self.opsystem)
+        print("Memory:", self.memory)
         print("Year:", self.year)
         print("Price:", self.value)  
-
 def main():
     apple = Computer("Apple", 2015, "OS11", 200.0)
     print(apple)
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -6,14 +6,16 @@ class ResaleShop:
     # Constructor
     def __init__(self, starting_balance: float) -> None:
          # You'll remove this when you fill out your constructor
+        self.inventory = []
         self.balance = starting_balance
     # Methods
     #creates a computer and adds it to the inventory, removes price from balance
-    def buy(self, name: str, price: float, year: int, OS: str, resale: float):
-        name = Computer(name, year, OS, resale)
-        self.inventory.append(name)
+    def buy(self, name: str, price: float, year: int, OS: str, resale: float, memory: int):
         if self.balance>= price: 
+            name = Computer(name, year, OS, resale, memory)
+            self.inventory.append(name)
             self.balance-=price
+            print("Computer Aquired!")
         else: 
             print("Insufficient Balance")
     #sells: adds the price to balance and removes the computer from the inventory
@@ -21,6 +23,7 @@ class ResaleShop:
         if computer in self.inventory: 
             self.balance+=computer.value
             self.inventory.remove(computer)
+            print("Computer sold!")
         else:
             print("Computer does not exist")
     #updates, price, OS, and if the computer is too old, removes it from the inventory
@@ -57,7 +60,7 @@ class ResaleShop:
         print(self.balance)
 def main():
     mystore = ResaleShop(500.00)
-    mystore.buy("c1", 200.00, 2018, "OS10", 250.00)
+    mystore.buy("c1", 200.00, 2018, "OS10", 250.00, 120)
     c1=mystore.inventory[0]
     mystore.printInventory()
     mystore.refurbish(c1, "OS11")
